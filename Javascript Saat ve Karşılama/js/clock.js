46fdef82a668730064fd3a43556582fb5d7c2d25
Esra Arslan <esrayildizarslan@gmail.com>
2022-08-25 13:32:04 +0300

    New Code

---
+++ Javascript Saat ve Karşılama/js/clock.js
@@ -0,0 +1,33 @@
+// İsim Yazdırma
+let name = prompt("Lutfen adinizi giriniz");
+let myName = document.querySelector("#myName");
+myName.innerHTML = `${name}`;
+
+// Saat yazdırma
+
+function showTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let days = [
+        "Pazar",
+        "Pazartesi",
+        "Salı",
+        "Çarşamba",
+        "Perşembe",
+        "Cuma",
+        "Cumartesi",
+      ];
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s + days[today.getDay()];
+    setTimeout(showTime, 1000);
+    
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  showTime()
